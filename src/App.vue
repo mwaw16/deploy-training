005405ed0b4634be0b65b9d0579a656b760cc54a
wawa <wawa@wawa.pl>
2022-09-22 11:36:11 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="wrapper">
+    <h1>Deploy training</h1>
+    <div class="color-wrapper">
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
+      <dpl-color></dpl-color>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script setup>
+  import dplColor from './components/dplColor.vue'
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .wrapper {
+    padding: 100px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  h1 {
+    text-align: center;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    color: purple;
+    margin-bottom: 50px;
   }
 
-  header .wrapper {
+  .color-wrapper {
     display: flex;
-    place-items: flex-start;
     flex-wrap: wrap;
+    gap: 20px;
   }
-}
 </style>
---
+++ src/components/dplColor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div @click="setColor" :style="dupa" class="dpl-color"></div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const dupa = ref('');
+
+  function setColor() {
+    dupa.value = `background-color: #${Math.floor(Math.random() * 16777215).toString(16)};`;
+  }
+</script>
+
+<style>
+  .dpl-color {
+    width: 60px;
+    height: 60px;
+    background-color: lightgray;
+    border-radius: 5px;
+    transition: background-color 0.3s ease-in-out;
+    cursor: pointer;
+  }
+</style>
